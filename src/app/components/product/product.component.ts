d439687c2af73230d78498783f07d83157acb353
Aurelius Wendelken <webtobesocial@gmail.com>
2018-06-10 00:24:40 +0200

    added suport for layout themes

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -26,6 +26,7 @@ import { Router } from '@angular/router';
 import { SplitPipe } from 'angular-pipes';
 import { TranslateService } from '@ngx-translate/core';
 import { DataService } from '../../services/data.service';
+import { LayoutService } from '../../services/layout.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from '../../../environments/environment';
 
@@ -40,6 +41,19 @@ declare var $: any;
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  backgroundColor: string;
+  headlineColor: string;
+  warningColor: string;
+  successColor: string;
+  navbarColor: string;
+  teaserColor: string;
+  buttonColor: string;
+  errorColor: string;
+  alertColor: string;
+  infoColor: string;
+  linkColor: string;
+  textColor: string;
+
   url: string;
   limit: number;
   page: number;
@@ -61,8 +75,22 @@ export class ProductComponent implements OnInit {
 
   constructor(private fb: FormBuilder,
     private http: Http,
+    private layout: LayoutService,
     private router: Router,
     private data: DataService) {
+    this.layout.currentBackgroundColor.subscribe(backgroundColor => this.backgroundColor = backgroundColor);
+    this.layout.currentHeadlineColor.subscribe(headlineColor => this.headlineColor = headlineColor);
+    this.layout.currentWarningColor.subscribe(warningColor => this.warningColor = warningColor);
+    this.layout.currentSuccessColor.subscribe(successColor => this.successColor = successColor);
+    this.layout.currentNavbarColor.subscribe(navbarColor => this.navbarColor = navbarColor);
+    this.layout.currentTeaserColor.subscribe(teaserColor => this.teaserColor = teaserColor);
+    this.layout.currentButtonColor.subscribe(buttonColor => this.buttonColor = buttonColor);
+    this.layout.currentAlertColor.subscribe(alertColor => this.alertColor = alertColor);
+    this.layout.currentErrorColor.subscribe(errorColor => this.errorColor = errorColor);
+    this.layout.currentTextColor.subscribe(textColor => this.textColor = textColor);
+    this.layout.currentInfoColor.subscribe(infoColor => this.infoColor = infoColor);
+    this.layout.currentLinkColor.subscribe(linkColor => this.linkColor = linkColor);
+
     this.token = localStorage.getItem('token');
     this.url = environment.apiUrl;
     this.headers = new Headers({
